a7b52225a3c272c81814828d9c4fdac792cec223
Iorgen Wildrik <w.drik@outlook.com>
2020-08-24 19:02:37 -0300

    feat: add Layout and MobileHeader components

---
+++ src/components/MobileHeader/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { FiMessageSquare } from 'react-icons/fi'
+
+export const Container = styled.div``;
+export const ProfileCircle = styled.img``;
+export const SearchInput = styled.div``;
+export const MessageIcon = styled(FiMessageSquare)``;
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,15 +5,18 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
+  } 
+
   html, body, #root {
     height: 100%;
     width: 100%;
     background: var(--color-background);
   }
+
   *, button, input {
     font-family: -apple-system,system-ui,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Fira Sans,Ubuntu,Oxygen,Oxygen Sans,Cantarell,Droid Sans,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Lucida Grande,Helvetica,Arial,sans-serif;
   }
+  
   :root {
     --color-header: #283e4a;
     --color-linkedin: #0077b5;
